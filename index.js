953ae17b6f986fcd6e2f48349d15fe04d5ca86d2
Michael Meoak <mmeoak240@gmail.com>
2022-01-18 18:27:51 -0700

    commit tree 67d7f9f14489076166462c2164596de7daa69c1e

---
+++ index.js
@@ -0,0 +1,20 @@
+
+function introduction(name) {
+  return `Hi, my name is ${name}.`;
+}
+
+name("Fred");
+
+function introductionWithLanguage(name, language) {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+name("Aki");
+language("Ember.js");
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+  return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+name(Aki);
+language()
